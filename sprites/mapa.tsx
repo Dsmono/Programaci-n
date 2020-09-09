8911368d291dea9185e0bb7ee8b4723cc5eb17f8
Dsmono <64215915+Dsmono@users.noreply.github.com>
2020-09-09 00:01:22 -0500

    Add files via upload

---
+++ sprites/mapa.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.08.05" name="mapa1" tilewidth="60" tileheight="60" tilecount="280" columns="35">
+ <image source="2.jpg" width="2100" height="490"/>
+</tileset>
